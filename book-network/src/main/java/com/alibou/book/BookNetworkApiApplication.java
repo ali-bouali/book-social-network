f2775fad339a31bab1fe3dd1feb624c989a1caff
ali-bouali <bouali.pro@gmail.com>
2024-02-13 13:37:09 +0100

    🎉 init spring boot project

---
+++ book-network/src/main/java/com/alibou/book/BookNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+}
