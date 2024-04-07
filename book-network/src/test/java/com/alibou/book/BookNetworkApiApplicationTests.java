d925bd4e5fcb27ca18354d92e619a683d612c9e9
ali-bouali <bouali.pro@gmail.com>
2024-04-07 12:58:04 +0200

    ✅ fixing tests [fake]

--- book-network/src/test/java/com/alibou/book/BookNetworkApiApplicationTests.java
+++ book-network/src/test/java/com/alibou/book/BookNetworkApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.alibou.book;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class BookNetworkApiApplicationTests {
 
 	@Test
