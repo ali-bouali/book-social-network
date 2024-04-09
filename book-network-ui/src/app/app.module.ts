21a0e39026735427b2d4b26343e6d836dd3575db
ali-bouali <bouali.pro@gmail.com>
2024-04-09 12:24:40 +0200

    💚 fix CORS attempt 2

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './pages/login/login.component';
+import {RegisterComponent} from './pages/register/register.component';
 import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 import {HttpTokenInterceptor} from './services/interceptor/http-token.interceptor';
-import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
 import {CodeInputModule} from 'angular-code-input';
 import {ApiModule} from './services/api.module';
 
@@ -19,23 +19,24 @@ import {ApiModule} from './services/api.module';
     RegisterComponent,
     ActivateAccountComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        HttpClientModule,
-        CodeInputModule,
-      ApiModule.forRoot({rootUrl: 'http://bsn-api:8088/api/v1'})
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CodeInputModule,
+    ApiModule.forRoot({rootUrl: 'http://localhost:8088/api/v1'})
+  ],
   providers: [
     HttpClient,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpTokenInterceptor,
       multi: true
-    },
+    }
 
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
